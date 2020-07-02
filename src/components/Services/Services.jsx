d54e73e7003473f47a84da79461fc742f3641e70
unknown <ms5978307@gmail.com>
2020-07-01 21:18:49 -0700

    added services component

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Services = () => {
+    return (
+        <div className='services'>
+            <div className='services-heading'></div>
+            <div className='services-features'>
+                <div className='services-feature'></div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
